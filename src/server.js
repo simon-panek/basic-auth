8d9dd7eeeffbada0d4239a39d2652a4dea547a50
Simon Panek <paneks19@gmail.com>
2020-12-14 23:07:33 -0800

    begining to write tests

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@
 const express = require('express');
 const app = express();
 const router = require('../auth/modules/routes.js');
+//const cors = require('cors');
 
 //middleware
-
+//app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
