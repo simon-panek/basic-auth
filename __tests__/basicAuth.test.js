a5047c5b38605f111cad23a48a0589a0b2c15e7f
Simon Panek <paneks19@gmail.com>
2020-12-16 08:31:28 -0800

    add basic auth middleware test, passing

--- __tests__/basicAuth.test.js
+++ __tests__/basicAuth.test.js
@@ -7,8 +7,20 @@ const user = new Users ();
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 const agent = supergoose(server);
+const auth = require('../auth/middleware/basicAuth.js');
+
+let users = { admin: { username: 'admin', password: 'password' }};
+
+beforeAll(async (done) => {
+  await new Users(users.admin).save();
+  done();
+});
 
 describe ('Auth middleware', () => {
+
+  // admin:password: YWRtaW46cGFzc3dvcmQ=
+  // admin:foo: YWRtaW46Zm9v
+
   it ('The middleware should function if sent a basic header', () => {
     const newUser = { 'username': 'dog', 'password': 'pass' };
     return agent.post('/signup').send(newUser)
@@ -16,7 +28,22 @@ describe ('Auth middleware', () => {
         expect(userInfo.body.username).toEqual('dog');
       })
   })
+
+  it ('logs in a user with the correct credentials', () => {
+    let req = { headers: { authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',},};
+    let res = {};
+    let next = jest.fn();
+    let middleware = auth;
+
+    return middleware(req, res, next)
+      .then(() => {
+        expect(next).toHaveBeenCalledWith();
+      });
+    
+  })
+
 })
+
 
 // describe ('Auth middleware', () => {
 //   it ('The the singin route should work as intended', () => {
